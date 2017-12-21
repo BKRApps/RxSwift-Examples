3a5a66590f41f4ef951c8a4af547ebab1ca55d2e
Kumar Reddy <kumarreddy72@gmail.com>
2017-12-22 00:48:20 +0530

    committing initial project

---
+++ RxSwift-Example/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RxSwift-Example
+//
+//  Created by Birapuram Kumar Reddy on 12/22/17.
+//  Copyright © 2017 KRiOSApps. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
